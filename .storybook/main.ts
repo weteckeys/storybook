bd0c96bd88884727e3176754aa4c06d4df65c8b6
WeTecKeys <94160962+sakthicsetce@users.noreply.github.com>
2023-08-18 01:20:59 +0530

    Addons installed commit

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from "@storybook/angular";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-storysource",
+    "@storybook/addon-highlight",
+    "@storybook/addon-a11y",
+    "@storybook/addon-styling"
+  ],
+  framework: {
+    name: "@storybook/angular",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ projects/library/src/lib/library.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-library',
+  template: `
+    <p>
+      library works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LibraryComponent {
+
+}
